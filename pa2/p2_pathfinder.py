699aa25f6e44276427fb3c0ae6dfbff67ab03159
Jeffrey Yao <jeyao@ucsc.edu>
2020-06-27 18:52:25 -0700

    rre

--- pa2/p2_pathfinder.py
+++ pa2/p2_pathfinder.py
@@ -1,3 +1,5 @@
+from heapq import heappop, heappush
+
 def heuristic(a, b):
 
     return abs(int(a[0]) - int(b[0])) + abs(int(a[1]) - int(b[1]))
@@ -17,50 +19,16 @@ def find_path (source_point, destination_point, mesh):
         A path (list of points) from source_point to destination_point if exists
         A list of boxes explored by the algorithm
     """
-        """
-      frontier = [] # our queue which we interpret  as a priority queue
-    heappush(frontier, (heuristic(initial_position,destination),initial_position) ) #putting our instial position with its cost
-    came_from = {} # where we have been
-    cost_so_far = {} #the cost so far
-    came_from[initial_position] = 0 #just setting up the intial destination
-    cost_so_far[initial_position] = 0 #where were starting
-    done = 0
-    while frontier:
-        current = heappop(frontier) # gets lowest priority
-        if current[1] == destination: # if it equals our goal destnation
-            done = 1
-            break
-        adjacents = adj(graph,current[1])# reset which variables were going to look at
-     #adj = navigation_edges(
-
-        for next in adjacents: # for all elements in adjacent to it
-            if next[0] in cost_so_far.keys():
-                if (next[1]+cost_so_far[current[1]]) <cost_so_far[next[0]]:
-                    cost_so_far[next[0]] = next[1] +cost_so_far[current[1]]
-                    came_from[next[0]] = current[1]
-            else:
-                cost_so_far[next[0]] =  next[1] +cost_so_far[current[1]]
-                came_from[next[0]] = current[1]
-                priority = cost_so_far[next[0]] + heuristic(next[0],destination)
-                heappush(frontier,(priority,next[0]))
-    if done == 1:
-
-        goback = []
-        current = destination
-        while current != 0:
-            goback.insert(0,current)
-
-            current = came_from[current]
-        return goback
-    return None     
-    pass    """
-
+    
     frontier = [(source_point, 0)]
     visited = []
     previous = {}
     distance = {}
     previous[source_point] = None
     distance[source_point] = 0
+
+    print("source point: ", source_point)
+    print("dest point: ", destination_point)
 
     while frontier:
         current_box = heappop(frontier)
@@ -73,14 +41,14 @@ def find_path (source_point, destination_point, mesh):
                 current_path = previous[current_path]
             return path
 
-        visited.append[current_box]
+        visited.append(current_box)
 
         for adjacent_box in mesh['adj'][current_box]:
             if adjacent_box in visited:
                 continue
             else:
-                
-                """move_cost[next_node] = new_cost
+
+                move_cost[next_node] = new_cost
                 temp_cost = new_cost
                 heappush(frontier, (next_node, temp_cost))
                 previous[next_node] = current_pos"""

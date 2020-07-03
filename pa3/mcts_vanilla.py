6cfa2fab0dfc9e002f4c58a84887a62ee27d929d
Sebastian Park <swpark@ucsc.edu>
2020-07-02 19:10:35 -0700

    swapping

--- pa3/mcts_vanilla.py
+++ pa3/mcts_vanilla.py
@@ -31,46 +31,58 @@ def traverse_nodes(node, board, state, identity):
     # Use highest UCB to choose each node down a path until leaf_node is reached
     turn = identity
     current = node
+    # index = 0
+    # while len(current.child_nodes) != 0:
+    #     for action in current.child_nodes:
+    #         current = current.child_nodes[action]
+    #         print("index: ", index)
+    #         print("action: ", action)
+    #         index += 1
     ucb = {}
 
     temp = node
     max_ucb = 0
 
     i = 0
-    for action in current.child_nodes:
-        print(i)
-        i = i +1
-        if current.child_nodes[action].visits == 0 :
-            #print(len(current.child_nodes))
-            child = current.child_nodes[action]
-            exploitation = child.wins/child.visits
+    # a = len(current.child_nodes)
+    while len(current.child_nodes) != 0:
+        print(len(current.child_nodes))
+        for action in current.child_nodes:
+            # print(i)
+            i = i +1
+            if current.child_nodes[action].visits == 0 :
+                #print(len(current.child_nodes))
+                current = current.child_nodes[action]
+                child = current.child_nodes[action]
+                exploitation = child.wins/child.visits
 
-            exploration = 2 * (sqrt((2 * log(child.parent.visits))/ child.visits))
+                exploration = 2 * (sqrt((2 * log(child.parent.visits))/ child.visits))
 
-            ucb[child] = exploitation + exploration
+                ucb[child] = exploitation + exploration
 
-            if ucb[child] > max_ucb:
-                max_ucb = ucb[child]
-                temp = child 
-        else:
-            continue
-            print("switch2")
-            child = current.child_nodes[action]
-            exploitation = 1 - (child.wins/child.visits)
+                if ucb[child] > max_ucb:
+                    max_ucb = ucb[child]
+                    temp = child 
+            else:
+                # continue
+                current = current.child_nodes[action]
+                print("switch2")
+                child = current.child_nodes[action]
+                exploitation = 1 - (child.wins/child.visits)
 
-            exploration = 2 * (sqrt((2 * log(child.parent.visits))/ child.visits))
+                exploration = 2 * (sqrt((2 * log(child.parent.visits))/ child.visits))
 
-            ucb[child] = exploitation + exploration
+                ucb[child] = exploitation + exploration
 
-            if ucb[child] < max_ucb:
-                max_ucb = ucb[child]
-                temp = child 
-
-        if turn == 1:
-            turn = 2
-        elif turn == 2:
-            turn = 1
-        current = current.child_nodes[action]
+                if ucb[child] < max_ucb:
+                    max_ucb = ucb[child]
+                    temp = child 
+            state = board.next_state(state, action)
+            if turn == 1:
+                turn = 2
+            elif turn == 2:
+                turn = 1
+            current = current.child_nodes[action]
         
 
     leaf_node = current
@@ -97,7 +109,7 @@ def expand_leaf(node, board, state):
     state2 = board.next_state(state, actions1[0])
     actions2 = board.legal_actions(state2)
     #print(actions[0])
-    print("this is the length before ",len(parent_node.child_nodes) )
+    # print("this is the length before ",len(parent_node.child_nodes) )
     child_node = MCTSNode(parent=parent_node, parent_action=actions1[0], action_list=board.legal_actions(state2))
     try:
         parent_node.child_nodes[actions2[0]] = child_node
@@ -105,7 +117,7 @@ def expand_leaf(node, board, state):
         return child_node
         parent_node.child_nodes[actions1[0]] = child_node
         print("it broke", actions2, "also :", actions1)
-    print("this is the length after",len(parent_node.child_nodes) )
+    # print("this is the length after",len(parent_node.child_nodes) )
     return child_node
 
     

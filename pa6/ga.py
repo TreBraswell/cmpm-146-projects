2b20a1689ba536a4b064d70cc9305eee2fa165c5
coagulantLegume <alvwolf@ucsc.edu>
2020-07-14 21:49:06 -0700

    further work on DE fitness, mutation, and ending condition

--- pa6/ga.py
+++ pa6/ga.py
@@ -179,8 +179,17 @@ class Individual_DE(object):
             penalties -= 2
 
         for item in list(filter(lambda de: de[1] == "0_hole", self.genome)): # connected holes
-            if len(list(filter(lambda de: (de[0] == item[0] + item[2] + 1) and (de[0] == "0_hole"), self.genome))) > 0:
-                penalties -= 2
+            try:
+                if self.to_level()[15][item[0] + item[2]] == "-":
+                    penalties -= 6
+            except:
+                    pass
+
+        for x in range(0,2): # check for cluttering around mario
+            for item in list(filter(lambda de: de[0] == x, self.genome)):
+                if item[1] != "4_block" or item[2] != 15:
+                    penalties -= 4
+                
         # STUDENT If you go for the FI-2POP extra credit, you can put constraint calculation in here too and cache it in a new entry in __slots__.
         self._fitness = sum(map(lambda m: coefficients[m] * measurements[m],
                                 coefficients)) + penalties
@@ -215,16 +224,29 @@ class Individual_DE(object):
                 y = de[2]
                 has_powerup = de[3]  # boolean
                 if choice < 0.33:
-                    x = offset_by_upto(x, width / 8, min=1, max=width - 2)
+                    try:
+                        count = 0
+                        while self.to_level()[y + 1][x] != "-" and x > 14 and count < 16:
+                            x = offset_by_upto(x, width / 8, min=1, max=width - 2)
+                            count += 1
+                    except:
+                        pass
                 elif choice < 0.66:
-                    y = offset_by_upto(y, height / 2, min=0, max=height - 1)
+                    try:
+                        count = 0
+                        while self.to_level()[y + 1][x] != "-" and count < 200:
+                            y = offset_by_upto(y, height / 2, min=0, max=height - 1)
+                            count += 1
+                    except:
+                        pass
                 else:
                     has_powerup = not de[3]
                 new_de = (x, de_type, y, has_powerup)
             elif de_type == "3_coin":
                 y = de[2]
                 if choice < 0.5:
-                    x = offset_by_upto(x, width / 8, min=1, max=width - 2)
+                    while x > 14:
+                            x = offset_by_upto(x, width / 8, min=1, max=width - 2)
                 else:
                     y = offset_by_upto(y, height / 2, min=0, max=height - 1)
                 new_de = (x, de_type, y)
@@ -238,8 +260,23 @@ class Individual_DE(object):
             elif de_type == "0_hole":
                 w = de[2]
                 if choice < 0.5:
-                    x = offset_by_upto(x, width / 8, min=1, max=width - 2)
+                    try:
+                        count = 0
+                        while self.to_level()[y][x + w] == "-" and count < 200:
+                            x = offset_by_upto(x, width / 8, min=1, max=width - 2)
+                            count += 1
+                    except:
+                        pass
                 else:
+                    try:
+                        count = 0
+                        while self.to_level()[y][x + w] == "-" and count < 8:
+                            x = offset_by_upto(x, width / 8, min=1, max=width - 2)
+                            count += 1
+                        else:
+                            x = offset_by_upto(x, width / 8, min=1, max=width - 2)
+                    except:
+                        pass
                     w = offset_by_upto(w, 4, min=1, max=width - 2)
                 new_de = (x, de_type, w)
             elif de_type == "6_stairs":
@@ -266,7 +303,16 @@ class Individual_DE(object):
                     madeof = random.choice(["?", "X", "B"])
                 new_de = (x, de_type, w, y, madeof)
             elif de_type == "2_enemy":
-                pass
+                x = de[0]
+                y = de[0]
+                if self.to_level()[15][x] == "X":
+                    try:
+                        count = 0
+                        while self.to_level()[y][x] != "X" and count < 200: 
+                            x = offset_by_upto(x, width / 8, min=1, max=width - 2)
+                            count += 1
+                    except:
+                        pass
             new_genome.pop(to_change)
             heapq.heappush(new_genome, new_de)
         return new_genome
@@ -365,10 +411,17 @@ def generate_successors(population):
         popTuple.append((population[i],population[i].fitness()))
     popTuple.sort(key = lambda x: x[1])
 
+    #roulette
     for i in range(0, len(popTuple)):
         if(random.randrange(0,len(popTuple) - 1) < i):
             selectIndiv.append(popTuple[i])
-    print("popTuple size: ", len(popTuple))
+
+    #elitism
+    b = int(len(popTuple)/2)
+    while b<len(popTuple):
+        temp= popTuple[b]
+        results.append(temp[0])
+        b = b +1
     
     for i in range(0, int(len(selectIndiv) / 2)): # pair selected individuals randomly and repopulate
         if len(selectIndiv[i][0].genome) == 0:
@@ -420,8 +473,8 @@ def ga():
             while True:
                 now = time.time()
                 # Print out statistics
+                best = max(population, key=Individual.fitness)
                 if generation > 0:
-                    best = max(population, key=Individual.fitness)
                     print("Generation:", str(generation))
                     print("Max fitness:", str(best.fitness()))
                     print("Average generation time:", (now - start) / generation)
@@ -432,6 +485,8 @@ def ga():
                 generation += 1
                 # STUDENT Determine stopping condition
                 stop_condition = False
+                if best.fitness() > 2.9 and metrics.metrics(best.to_level())['solvability'] == 1:
+                    stop_condition = True
                 if stop_condition:
                     break
                 # STUDENT Also consider using FI-2POP as in the Sorenson & Pasquier paper
@@ -458,6 +513,6 @@ if __name__ == "__main__":
     now = time.strftime("%m_%d_%H_%M_%S")
     # STUDENT You can change this if you want to blast out the whole generation, or ten random samples, or...
     for k in range(0, 10):
-        with open("levels/" + now + "_" + str(k) + ".txt", 'w') as f:
+        with open( now + "_" + str(k) + ".txt", 'w') as f:
             for row in final_gen[k].to_level():
                 f.write("".join(row) + "\n")
